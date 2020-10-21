e1502253ac009fcf997a56094f4b2182e31f39e5
Sergey Tsehmister <sergeytsehmister@gmail.com>
2020-10-21 17:16:00 +0300

    First part app start

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className = 'header'>
+    <img src='https://www.iconarchive.com/download/i107110/roundicons/100-free-solid/plant-flower.ico'/>
+  </header>
+}
+
+export default Header;
